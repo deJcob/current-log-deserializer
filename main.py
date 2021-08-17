fc4785b6431da6f16cc148cac5540e78823d666e
Jakub Kakiel <kubakakiel@gmail.com>
2021-08-17 22:59:33 +0200

    Changed from ndarray to basic types (float64, int64, int32)

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def print_hi(name):
 if __name__ == '__main__':
     with open("test_file", "rb") as f:
         byte = f.read(8)
-        timestamp = np.frombuffer(byte, dtype='int64')
+        timestamp = np.frombuffer(byte, dtype='int64')[0]
         print(timestamp)
         byte = f.read(4)
-        size = np.frombuffer(byte, dtype='int32')
+        size = np.frombuffer(byte, dtype='int32')[0]
         byte = f.read(4)
-        print(size[0])
-        for i in range(size[0]):
+        print(size)
+        for i in range(size):
             byte1 = f.read(8)
             byte2 = f.read(8)
-            print(np.frombuffer(byte1, dtype='float64'), np.frombuffer(byte2, dtype='float64'))
+            print(np.frombuffer(byte1, dtype='float64')[0], np.frombuffer(byte2, dtype='float64')[0])
 
         while byte != b"":
             # print(byte)
