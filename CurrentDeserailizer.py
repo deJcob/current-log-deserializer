2f07c306356e389cb81743a19c07e3c5a6a8b130
Jakub Kakiel <kubakakiel@gmail.com>
2021-08-18 23:20:55 +0200

    Comments, readme

--- CurrentDeserailizer.py
+++ CurrentDeserailizer.py
@@ -15,27 +15,27 @@ def read_log(name, start_in_zero = True):
 
     with open(name, "rb") as f:
 
-        byte = f.read(8)
-        timestamp_1 = np.frombuffer(byte, dtype='int64')[0]/(10**9)
+        byte = f.read(8)                                            # Timepstamp is uint64_t
+        timestamp_1 = np.frombuffer(byte, dtype='int64')[0]/(10**9) # nsec to sec
 
-        if start_in_zero:
+        if start_in_zero:                       # True if you want to normalize time from 0
             data.timestamp.append(0)
-        else:
+        else:                                   # Time from ros.time.now().ToNsec
             data.timestamp.append(timestamp_1)
 
         while byte != b"":
             byte = f.read(4)
-            size = np.frombuffer(byte, dtype='int32')[0]
-            f.read(4)
+            size = np.frombuffer(byte, dtype='int32')[0] # size is size_t type
+            f.read(4)                                    # always zeros
             for i in range(size):
                 data.currentA.append(np.frombuffer(f.read(8), dtype='float64')[0])
                 data.currentB.append(np.frombuffer(f.read(8), dtype='float64')[0])
 
             byte = f.read(8)
-            if byte == b"":
+            if byte == b"":    # if there is no next timestamp there is no more data
                 for i in range(size):
                     data.timestamp.append(data.timestamp[-1] + time_delta)
-                data.timestamp.pop(-1)
+                data.timestamp.pop(-1)                       # last element should be removed
                 break
             timestamp_2 = np.frombuffer(byte, dtype='int64')[0]/(10**9)
             time_delta = (timestamp_2 - timestamp_1) / size
--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-Notes
+# How to use 
 
-``
-hexdump -C test_file -n 5000
-``
+The read_log method opens file and returns array of timestamps, current for channel A and channel B. 
 
+In ``main.py`` there is demo based on ``matplolib``.
 
 # Required packages
 
 - Numpy
 - Matplolib (only for demo puropses)
+
+# Current data log file
+
+File is builded with multiple of dataset:
+
+- Timestamp from ros.time.Now().ToNsecs() (uint64_t)
+- Size of vector (size_t, 4 bytes)
+- Empty 4 bytes
+- Vector of pairs of doubles (two channels)
+
+To check the structure of file you may use:
+```
+hexdump -C test_file -n 5000
+```
