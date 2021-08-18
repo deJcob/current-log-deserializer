fe02b641a8137ca7a735b3ecc4873ece06ae3bf2
Jakub Kakiel <kubakakiel@gmail.com>
2021-08-18 23:10:57 +0200

    Function moved to separeted file

--- main.py
+++ main.py
@@ -1,52 +1,5 @@
-import numpy as np
+from CurrentDeserailizer import read_log
 from matplotlib import pyplot as plt
-
-
-class CurrentData:
-
-    def __init__(self):
-        self.timestamp = []
-        self.currentA = []
-        self.currentB = []
-
-
-def read_log(name, start_in_zero = True):
-
-    data = CurrentData()
-
-    with open(name, "rb") as f:
-
-        byte = f.read(8)
-        timestamp_1 = np.frombuffer(byte, dtype='int64')[0]/(10**9)
-
-        if start_in_zero:
-            data.timestamp.append(0)
-        else:
-            data.timestamp.append(timestamp_1)
-
-        while byte != b"":
-            byte = f.read(4)
-            size = np.frombuffer(byte, dtype='int32')[0]
-            f.read(4)
-            for i in range(size):
-                data.currentA.append(np.frombuffer(f.read(8), dtype='float64')[0])
-                data.currentB.append(np.frombuffer(f.read(8), dtype='float64')[0])
-
-            byte = f.read(8)
-            if byte == b"":
-                for i in range(size):
-                    data.timestamp.append(data.timestamp[-1] + time_delta)
-                data.timestamp.pop(-1)
-                break
-            timestamp_2 = np.frombuffer(byte, dtype='int64')[0]/(10**9)
-            time_delta = (timestamp_2 - timestamp_1) / size
-
-            for i in range(size):
-                data.timestamp.append(data.timestamp[-1] + time_delta)
-            timestamp_1 = timestamp_2
-
-    return data
-
 
 if __name__ == '__main__':
     temp = read_log('test_file')
